e23de085867dc07adfa1d8d6c05bf298bb47a807
Veeresh Rex <52687597+Veeresh-Rex@users.noreply.github.com>
2020-06-22 11:27:40 +0530

    Updates

---
+++ precache-manifest.cf53b9711a17dc0eef74eda14db6b371.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f7dd69cf91c6c5339e0022f2c9d88c9",
+    "url": "/colorpaletts/index.html"
+  },
+  {
+    "revision": "a121ae2ded140fabed83",
+    "url": "/colorpaletts/static/css/main.9f926f04.chunk.css"
+  },
+  {
+    "revision": "8f710686a35792e51b95",
+    "url": "/colorpaletts/static/js/2.ce700570.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/colorpaletts/static/js/2.ce700570.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a121ae2ded140fabed83",
+    "url": "/colorpaletts/static/js/main.15edcc5b.chunk.js"
+  },
+  {
+    "revision": "97e6bf64cc3cce943c21",
+    "url": "/colorpaletts/static/js/runtime-main.74a09303.js"
+  }
+]);
